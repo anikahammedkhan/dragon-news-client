79ec1c8398d9e295ad3dbc05cf6f82d847fd74ea
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-10-22 15:12:58 +0600

    rightside added

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -3,7 +3,12 @@ import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { FcGoogle } from 'react-icons/fc';
-import { BsGithub } from 'react-icons/bs';
+import { BsGithub, BsFacebook, BsTwitter, BsWhatsapp, BsInbox } from 'react-icons/bs';
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from '../../../Assets/carousel1.jpg';
+import img2 from '../../../Assets/carousel2.jpg';
+
+
 const RightSideNav = () => {
     return (
         <div>
@@ -11,15 +16,33 @@ const RightSideNav = () => {
                 <Button variant="outline-primary"><FcGoogle /> Log In with Google</Button>
                 <Button variant="outline-dark"><BsGithub /> Log In with Github</Button>
             </ButtonGroup>
-            <div>
+            <div className='mt-4'>
                 <h5>Find Us On</h5>
                 <ListGroup>
-                    <ListGroup.Item>Facebook</ListGroup.Item>
-                    <ListGroup.Item>Twiteer</ListGroup.Item>
-                    <ListGroup.Item>Github</ListGroup.Item>
-                    <ListGroup.Item>Whats App</ListGroup.Item>
-                    <ListGroup.Item>Email</ListGroup.Item>
+                    <ListGroup.Item><BsFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item><BsTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item><BsGithub /> Github</ListGroup.Item>
+                    <ListGroup.Item><BsWhatsapp /> Whats App</ListGroup.Item>
+                    <ListGroup.Item><BsInbox /> Email</ListGroup.Item>
                 </ListGroup>
+            </div>
+            <div className='mt-4'>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={img1}
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={img2}
+                            alt="Second slide"
+                        />
+                    </Carousel.Item>
+                </Carousel>
             </div>
         </div>
     );
